8a5e8698337dfaeaa8c70f4fd79bdd170c5da213
fairyly <fairyilys@gmail.com>
2019-04-13 18:25:57 +0800

    feat: build

---
+++ build/precache-manifest.0d3d0b24245ceacd7207663bb0ff36f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c58f21877ba589ecfa42",
+    "url": "/static/js/main.5940efee.chunk.js"
+  },
+  {
+    "revision": "71f8aa4fb23b0fc3b487",
+    "url": "/static/js/2.336bcb27.chunk.js"
+  },
+  {
+    "revision": "c58f21877ba589ecfa42",
+    "url": "/static/css/main.9877e4c7.chunk.css"
+  },
+  {
+    "revision": "71f8aa4fb23b0fc3b487",
+    "url": "/static/css/2.d428b0df.chunk.css"
+  },
+  {
+    "revision": "27b0dfdeed6b3723928535f2a1e7fde8",
+    "url": "/index.html"
+  }
+];
