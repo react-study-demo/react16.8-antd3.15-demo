9c3db8f2b2749002ea6bc92f0edae6fc69af1d7f
fairyly <fairyilys@gmail.com>
2019-04-13 21:28:59 +0800

    feat: 增加homepage

---
+++ build/precache-manifest.6d4271d58d3ce6cb645aee0ff445534a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "c82e53e8f8ac28d4eef4",
+    "url": "./static/js/main.bb9366d5.chunk.js"
+  },
+  {
+    "revision": "b6d33240adb569e5365b",
+    "url": "./static/js/2.63c2985a.chunk.js"
+  },
+  {
+    "revision": "c82e53e8f8ac28d4eef4",
+    "url": "./static/css/main.2ed5165b.chunk.css"
+  },
+  {
+    "revision": "b6d33240adb569e5365b",
+    "url": "./static/css/2.d428b0df.chunk.css"
+  },
+  {
+    "revision": "81f910985a0be5ce5878b2fce0404b1f",
+    "url": "./index.html"
+  }
+];
